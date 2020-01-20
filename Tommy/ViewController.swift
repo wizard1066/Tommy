903d18349a3a5e9114abd5e85b2cf29c6255afd3
Mark Lucking <mark.lucking@gmail.com>
2020-01-20 09:49:54 +0100

    Initial Commit

---
+++ Tommy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tommy
+//
+//  Created by localadmin on 20.01.20.
+//  Copyright © 2020 Mark Lucking. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
